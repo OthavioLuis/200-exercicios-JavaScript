a58dce8703a4e20a52502c2cc1be07eda8bd2806
OthavioLuis <othaviorodriguesf@gmail.com>
2025-03-04 15:31:04 -0300

    Ex 01 de funções - Callback

---
+++ Funções/Callback.js
@@ -0,0 +1,10 @@
+// Crie uma função que recebe uma função de callback e a executa após 2 segundos.
+
+function pausaDramatica() {
+    console.log("tarammm")
+}
+
+setTimeout(pausaDramatica, 2000) //2000 = 2 segundos
+
+//uma observação para mim, caso fosse chamada a função com os parenteses (), ela rodaria na hr, com o callback, ela pode ser chamada e ser alterada quando for executada
+
